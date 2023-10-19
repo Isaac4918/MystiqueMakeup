3cd2282716069ac34b0596eec513285abaf21a66
Mariana <mfernandezmar3@gmail.com>
2023-10-19 17:34:42 -0600

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; 
+import Home from "./pages/Home"; 
+import LoginRegister from "./pages/LoginRegister";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" exact component={Home}/>
-        </Routes>
-      </Router>
-    </div>
-    
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginRegister />} />
+      </Routes>
+    </Router>
   );
 }
 
