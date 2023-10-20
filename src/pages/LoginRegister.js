d6085dc9787a09c92d08ce57fcc3dcc1c2d8e53a
Mariana <mfernandezmar3@gmail.com>
2023-10-19 20:06:14 -0600

    loginRegister screen finished

--- src/pages/LoginRegister.js
+++ src/pages/LoginRegister.js
@@ -1,57 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Register from './Register';
+import Login from './Login'; 
 import '../styles/LoginRegister.css';
 
-function Registro() {
-    const [datos, setDatos] = useState({
-        usuario: '',
-        correo: '',
-        contraseña: ''
-    });
-
-    const handleInputChange = (event) => {
-        setDatos({
-            ...datos,
-            [event.target.name] : event.target.value
-        })
-    }
-
-    const enviarDatos = (event) => {
-        event.preventDefault();
-        console.log(datos.usuario +' '+ datos.correo +' '+datos.contraseña)
-    }
-
+function LoginRegister() {
     return (
-        <div className="Registro">
-            <form onSubmit={enviarDatos}>
-                <h1>Registrar cuenta</h1>
-                <label>Usuario</label>
-                <br />
-                <input
-                    type='text'
-                    onChange={handleInputChange}
-                    name='username'
-                />
-                <br />
-                <label>Correo electrónico</label>
-                <br />
-                <input
-                    type='email'
-                    onChange={handleInputChange}
-                    name='email'
-                />
-                <br />
-                <label>Contraseña</label>
-                <br />
-                <input
-                    type='password'
-                    onChange={handleInputChange}
-                    name='password'
-                />
-                <br />
-                <button type='submit'>Crear Cuenta</button>
-            </form>
+        <div className='LoginRegister'>
+            <Register />
+            <Login />
         </div>
     );
 }
 
-export default Registro;
+export default LoginRegister;
