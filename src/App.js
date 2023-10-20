d3f17bf16152cc29bf5f21578b89bc848fa9f71d
Mariana <mfernandezmar3@gmail.com>
2023-10-19 18:58:28 -0600

    some changes of style

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginRegister />} />
+        <Route path="/LoginRegister" element={<LoginRegister />} />
       </Routes>
     </Router>
   );
