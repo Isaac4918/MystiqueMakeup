eff205db89d97ded600529079ead23fd2ba331db
MariLau-RE <marilaure29@estudiantec.cr>
2023-10-28 00:26:46 -0600

    Git Ignore

---
+++ src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/Cart.css'; 
+
+function Cart() {
+  return (
+    <div className="Cart">
+      <h1>Productos  Precio  Cantidad</h1>
+    </div>
+  );
+}
+
+export default Cart;
