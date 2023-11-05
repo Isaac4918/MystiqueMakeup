610038da7187e6584c1e3856cf71e90cd52b50a4
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-11-04 22:30:42 -0600

    added functionality to create publication page

--- src/pages/publicationPages/CreatePublication.js
+++ src/pages/publicationPages/CreatePublication.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import "../../styles/Publications.css";
 import Navbar from "../../components/Navbar";
 import back from "../../components/assets/arrowBack.png";
@@ -8,25 +8,44 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import imagePlaceholder from '../../components/assets/imagePlaceHolder.png';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { UpdateSubcategories } from '../categoryPages/ModifyCategory';
 
 const CreatePublication = () => {
     // VARIABLES -----------------------------------------------------------------
     const navigate = useNavigate();
     const hiddenFileInput = useRef(null);
 
+    const baseAPIurl = 'http://localhost:5000';
     const [selectedName, setSelectedName] = useState('');
+    const [blobImage, setBlobImage] = useState('-');
     const [selectedDescription, setSelectedDescription] = useState('');
     const [selectedTags, setSelectedTags] = useState('');
-
     const [selectedImage, setImage] = useState(imagePlaceholder);
-    const [imageURL, setURL] = useState('');
     const [selectedCategory, setSelectedCategory] = useState('');
     const [selectedSubcategory, setSelectedSubcategory] = useState('');
-
-    const categories = ["Disney", "Spooky"];
-    const subcategories = ["Villanos", "Magia"];
+    const [categories, setCategories] = useState([]);
+    const [parsedCategories, setParsedCategories] = useState([]);
+    const [subcategories, setSubcategories] = useState([]);
 
     // FUNCTIONS -----------------------------------------------------------------
+
+    const getCategories = async () => {
+        const response = await fetch(baseAPIurl + '/category/all', {
+            method: 'GET',
+        }).then(res => res.json());
+        setCategories(response);
+        //console.log("****",response);
+        let categorylist = [];
+        for (let i = 0; i < response.length; i++) {
+            categorylist.push(response[i].name);
+        }
+        setParsedCategories(categorylist);
+        //return;
+    }
+    useEffect(() => {
+        getCategories();
+    }, []);
+
     const handleClickImage = (event) => {
         hiddenFileInput.current.click();
     };
@@ -35,13 +54,43 @@ const CreatePublication = () => {
         const fileUploaded = event.target.files;
         if (fileUploaded[0].name) {
             setImage(URL.createObjectURL(fileUploaded[0]));
+            setBlobImage(fileUploaded[0]);
+            //console.log("====>", blobImage);
         }
     };
 
-    const handlePublication = (event) => {
+    const getSubcategories = (pCategory) => {
+        let subcategorylist = [];
+        for (let i = 0; i < categories.length; i++) {
+            if (categories[i].name === pCategory) {
+                for (let j = 0; j < categories[i].subCategories.length; j++) {
+                    subcategorylist.push(categories[i].subCategories[j].name);
+                }
+            }
+        }
+        setSubcategories(subcategorylist);
+    }
+
+    const prueba = async (event) => {
+        //console.log("====>", blobImage);
+
+        // console.log("categories =====>", categories);
+        // console.log("parsedCategories =====>", parsedCategories);
+        // console.log("selectedTags =====>", selectedTags.split(/[# ,]+/).map((tag) => tag.trim()).slice(1));
+
+
+        // let pruebaArray = [1,2,3,4];
+        // pruebaArray = [...pruebaArray.slice(1)]
+        // console.log("pruebaArray =====>", pruebaArray);
+
+        //pTags.split("#").map((tag) => tag.trim())
+
+    }
+
+    const handlePublication = async(event) => {
         event.preventDefault();
 
-        if (!selectedName || !selectedDescription|| !selectedTags|| !selectedCategory || !selectedSubcategory || !selectedImage) {
+        if (!selectedName || !selectedDescription || !selectedTags || !selectedCategory || !selectedSubcategory || !selectedImage) {
             alert("ERROR: Todos los campos son obligatorios");
             return;
         }
@@ -66,51 +115,94 @@ const CreatePublication = () => {
             return;
         }
 
-        if(selectedImage === imagePlaceholder){
+        if (selectedImage === imagePlaceholder) {
+            alert("ERROR: Debe seleccionar una imagen");
+            return;
+        }
+        if (blobImage === '-') {
             alert("ERROR: Debe seleccionar una imagen");
             return;
         }
 
-        createPublication(1, selectedName, selectedDescription, selectedImage, selectedTags, selectedCategory, selectedSubcategory, imageURL);
+        let result = await createPublication(selectedName, selectedDescription, selectedTags, selectedCategory, selectedSubcategory);
+        if (result.status === 200) {
+            alert("Publicación creada con éxito");
+            navigate('/PublicationManagement');
+        } else {
+            alert("ERROR: No se pudo crear la publicación");
+        }
     };
 
-    const createPublication = async (pId, pName, pDescription, pImagePath, pTags, pCategory, pSubcategory, pImageURL) => {
+    const createPublication = async (pName, pDescription, pTags, pCategory, pSubcategory) => {
         const date = new Date();
         const year = date.getFullYear();
         const month = date.getMonth() + 1; // months are zero indexed
         const day = date.getDate();
         const formattedDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
 
-        const newData = await fetch('http://localhost:5000/publications/create',{
+        // get id
+        const currentId = await fetch(baseAPIurl + '/publications/get/id', {
+            method: 'GET',
+        }).then(res => res.json());
+
+        // update the id
+        const nextId = await fetch(baseAPIurl + '/publications/update/id', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }, body: JSON.stringify({
+                id: currentId + 1
+            })
+        });
+
+        // upload the image
+
+        let formData = new FormData();
+        formData.append('image', blobImage)
+
+        const uploadImage = await fetch(baseAPIurl + '/image/upload/publications/' + currentId.toString(), {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json'
+            },
+            body: formData
+        }).then(res => res.json());
+
+        // save the url of the image
+        let imageURL = uploadImage.imageUrl;
+
+        //define the path of the image
+        let imagePath = 'publications/' + currentId.toString();
+
+        // create the publication
+        const newData = await fetch(baseAPIurl + '/publications/create',{
             method: 'POST',
             headers : {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
             body: JSON.stringify({
-                id: pId,
+                id: currentId,
                 name: pName,
                 description: pDescription,
-                imagePath: pImagePath,
+                imagePath: imagePath,
                 date: formattedDate,
-                tags: pTags.split("#").map((tag) => tag.trim()),
+                tags: pTags.split(/[# ,]+/).map((tag) => tag.trim()).slice(1),
                 category: pCategory,
                 subcategory: pSubcategory,
-                imageURL: ""
+                imageURL: imageURL
             })
-        }).then(res => res.json())
-        console.log(newData.response);
-        if(newData.response === 'Publication created successfully'){
-            alert('Publicación creada con éxito');
-            navigate('/PublicationManagement');
-        }
+        });
+        
+        return newData;
     }
 
     // RETURN -----------------------------------------------------------------
     return (
         <div className="ModifyCreatePublication">
             <Navbar showIcons={true} />
-            <Link to={"/PublicationManagement"}><button className="backManagementPublication"><img src={back} alt=""/></button></Link>
+            <Link to={"/PublicationManagement"}><button className="backManagementPublication"><img src={back} alt="" /></button></Link>
             <section className="layoutModifyCreatePublication">
                 <div className="gridPosition">
                     <label>Nombre</label><br />
@@ -126,7 +218,7 @@ const CreatePublication = () => {
                     <input value={selectedTags} onChange={(e) => setSelectedTags(e.target.value)} type="text" id="tagsPublication" name="tags" /><br />
 
                     <label>Categoría</label><br />
-                    <Dropdown value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} options={categories} placeholder="Seleccione una opción" className="options" />
+                    <Dropdown value={selectedCategory} onChange={(e) => { setSelectedCategory(e.target.value); getSubcategories(e.target.value); }} options={parsedCategories} placeholder="Seleccione una opción" className="options" />
                     <br />
 
                     <label>Subcategoría</label><br />
