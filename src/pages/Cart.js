4e700cf5901e5d1c0f6b92697634c1655eb2e70e
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-11-06 17:57:36 -0600

    implemented cart and partially implemented purchase

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -14,48 +14,48 @@ export function Back() {
   )
 }
 
-export function OpenPayment() {
-  return (
-    <div>
-      <a href="/PaymentDetails"><button className="buttonPay">Pagar</button></a>
-    </div>
-  )
-}
-
-
-
-
-const calculateTotal = (products) => {
-  return products.reduce((total, product) => total + product.price * product.available, 0);
-};
-
-const ModifyQuantity = ({ available, onIncrement, onDecrement }) => {
-
-  return (
-    <div>
-      <div className="buttonIncrement">
-        <div className="available">
-          <button onClick={onDecrement}>-</button>
-
-          <span>{available}</span>
-
-          <button onClick={onIncrement}>+</button>
-        </div>
-      </div>
-    </div>
-
-  );
-
-};
-
 function Cart() {
-  const [productList, setProductList] = useState({});
+  const [productList, setProductList] = useState([]);
+  const baseAPIurl = 'http://localhost:5000';
+  const [totalPrice, setTotalPrice] = useState(0);
   //const [total, setTotal] = useState(calculateTotal({}));
   //const[cart, setCart] = useState({})
   let username = localStorage.getItem('username');
 
+  const ModifyQuantity = ({ quantity, onIncrement, onDecrement }) => {
+    //let number = available - (available - 1);
+    return (
+      <div>
+        <div className="buttonIncrement">
+          <div className="available">
+            <button onClick={onDecrement}>-</button>
+  
+            <label onChange={calculateTotal}>{quantity}</label>
+  
+            <button onClick={onIncrement}>+</button>
+          </div>
+        </div>
+      </div>
+  
+    );
+  
+  };
+
+  const OpenPayment = () => {
+    return (
+      <div>
+        <a href="/PaymentDetails"><button onClick={updateCart} className="buttonPay">Pagar</button></a>
+      </div>
+    )
+  }
+
+  const calculateTotal = () => {
+    setTotalPrice(productList.reduce((total, product) => total + product.price * product.quantity, 0));
+  };
+
   const getCart = async () => {
-    const response = await fetch('http://localhost:5000/shoppingCart/get', {
+    console.log("USERNAME", username);
+    const response = await fetch(baseAPIurl + '/shoppingCart/get', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -66,38 +66,64 @@ function Cart() {
     console.log("CARRITO", response);
     console.log("PRODUCTO", response.products)
     setProductList(response.products)
+    let totalvar = 0;
+    for (let i = 0; i < response.products.length; i++) {
+      totalvar += response.products[i].price * response.products[i].quantity;
+    }
+    setTotalPrice(totalvar);
   }
 
   const onIncrement = (index) => {
     setProductList((prevProducts) => {
-      const updatedProduct = { ...prevProducts[index], available: prevProducts[index].available + 1 };
-      const updatedProducts = [...prevProducts];
-      updatedProducts[index] = updatedProduct;
-      //setTotal(calculateTotal(updatedProducts));
-      return updatedProducts;
+      if(prevProducts[index].available > prevProducts[index].quantity){
+        const updatedProduct = { ...prevProducts[index], quantity: prevProducts[index].quantity + 1 };
+        const updatedProducts = [...prevProducts];
+        updatedProducts[index] = updatedProduct;
+        return updatedProducts;
+      }else{
+        return prevProducts;
+      }
     });
   };
 
   const onDecrement = (index) => {
     setProductList((prevProducts) => {
-      const updatedProduct = { ...prevProducts[index], available: prevProducts[index].available - 1 };
+      const updatedProduct = { ...prevProducts[index], quantity: prevProducts[index].quantity - 1 };
       const updatedProducts = [...prevProducts];
       updatedProducts[index] = updatedProduct;
 
       // If the available is zero, remove the product from the cart
-      if (updatedProduct.available === 0) {
+      if (updatedProduct.quantity === 0) {
         updatedProducts.splice(index, 1);
+        //borrar de la base
       }
-
-      //setTotal(calculateTotal(updatedProducts));
       return updatedProducts;
     });
   };
 
+  const updateCart = async() =>{
+    const response = await fetch(baseAPIurl + '/shoppingCart/update', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            products: productList
+        })
+    });
+}
+
 
   useEffect(() => {
     getCart();
+    calculateTotal();
   }, []);
+
+  useEffect(() => {
+    calculateTotal();
+  }, [productList]);
 
 
 
@@ -120,29 +146,29 @@ function Cart() {
             </thead>
             <tbody>
               {Array.isArray(productList) && productList.map((product, index) => {
-                return(                
-                <tr key={product.name}>
-                  <div className="imageContentCart">
-                    <div className="cardImageCart">
-                    <td><img src={product.imageURL} alt={product.name} /></td>
-                    </div>
+                return (
+                  <tr key={product.name}>
+                    <div className="imageContentCart">
+                      <div className="cardImageCart">
+                        <td><img src={product.imageURL} alt={product.name} /></td>
+                      </div>
                     </div>
                     <td>{product.name}</td>
                     <td>{product.price}</td>
                     <td>
-                      <ModifyQuantity
-                        available={1}
+                    <ModifyQuantity
+                        quantity={product.quantity}
                         onIncrement={() => onIncrement(index)}
                         onDecrement={() => onDecrement(index)}
                       />
                     </td>
-                  <td>{product.price * (product.available-(product.available-1))}</td>
-                </tr>)
+                    <td>{product.price * product.quantity}</td>
+                  </tr>)
               })}
             </tbody>
           </table>
 
-          <h2>Total: ₡{0}</h2>
+          <h2>Total: ₡{totalPrice}</h2>
           <div className="buttonPay">
             <OpenPayment />
           </div>
@@ -151,5 +177,9 @@ function Cart() {
     </div>
   );
 }
+
+{/*
+
+*/}
 
 export default Cart;
