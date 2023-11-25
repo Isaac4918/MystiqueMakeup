67a1ff5419a4623bd79cacf9a0071fbadbcc7fe0
aleedca <acerdas1701@estudiantec.cr>
2023-11-24 23:07:07 -0600

    table requested makeups done

--- src/pages/Delivers.js
+++ src/pages/Delivers.js
@@ -1,69 +1,73 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import '../styles/Calendar.css';
 import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import { red } from '@mui/material/colors';
 
 // Se le va a pasar la info de purchase 
 // Se tendra que actualizar el estado de scheduled a true
 // Y el delivery date se tendra que actualizar con la fecha que se elija en el calendario
 
-const Delivers = props => {
+function Delivers() {
+  const baseAPIurl = 'http://localhost:5000';
+  const [selectedRequestedMakeups, setRequestedMakeups] = useState([]);
 
-const data = { //Este es un ejemplo agregado a mano para mostrar los pedidos pendientes
-  columns: [ // las columnas se mantienen ya que es la info que queremos mostrar
-    {
-      label: 'No. Solicitud',
-      field: 'requestedNumber',
-      sort: 'asc'
-    },
-    {
-      label: 'Usuario',
-      field: 'username',
-      sort: 'asc'
-    },
-    {
-      label: 'Maquillaje',
-      field: 'makeup',
-      sort: 'asc'
-    },
-    {
-      label: 'Día solicitado',
-      field: 'requestedDate',
-      sort: 'asc'
-      }
-  ],
-  rows: [
-    { // Y estos son los datos de los purchases que se van a mostrar
-        requestedNumber: 1,
-        username: "mari0502",
-        makeup: "Mariposa",
-        requestedDate: "06/11/23",
- 
-    },
-    { 
-        requestedNumber: 2,
-        username: "Marilau123",
-        makeup: "Boda",
-        requestedDate: "07/11/23",
- 
-    },
-    { 
-        requestedNumber: 3,
-        username: "Marilau123",
-        makeup: "Bruja",
-        requestedDate: "08/11/23",
- 
-    },
-    { 
-        requestedNumber: 4,
-        username: "Aleeee",
-        makeup: "Mariposa",
-        requestedDate: "08/11/23",
- 
+  useEffect(() => {
+    const getRequestedMakeups = async () => {
+      const response = await fetch(baseAPIurl + '/publications/request/get/all', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }).then(res => res.json());
+      setRequestedMakeups(response);
     }
-  ]
-};
+  
+    getRequestedMakeups();
+  }, []);
 
-return (
-    <MDBTable scrollY>
+
+  const data = { //Este es un ejemplo agregado a mano para mostrar los pedidos pendientes
+    columns: [ // las columnas se mantienen ya que es la info que queremos mostrar
+      {
+        label: 'No. Solicitud',
+        field: 'requestedNumber',
+        sort: 'asc'
+      },
+      {
+        label: 'Usuario',
+        field: 'username',
+        sort: 'asc'
+      },
+      {
+        label: 'Maquillaje',
+        field: 'makeup',
+        sort: 'asc'
+      },
+      {
+        label: 'Día solicitado',
+        field: 'requestedDate',
+        sort: 'asc'
+      },
+      {
+        label: 'Estado',
+        field: 'scheduled',
+        sort: 'asc'
+      }
+    ],
+    rows: selectedRequestedMakeups && selectedRequestedMakeups.map(requestedMakeup => {
+      return {
+        requestedNumber: requestedMakeup.orderNumber,
+        username: requestedMakeup.username,
+        makeup: requestedMakeup.makeup,
+        requestedDate: requestedMakeup.requestedDate,
+        scheduled: <td className="rowCustomize" style={{color: requestedMakeup.scheduled === "Pendiente" ? "rgb(192, 25, 25)" : "green"}}>{requestedMakeup.scheduled}</td>
+      }
+    })
+  };
+
+  return (
+    <MDBTable scrollY className="coloredHeader">
       <MDBTableHead columns={data.columns} />
       <MDBTableBody rows={data.rows} />
     </MDBTable>
